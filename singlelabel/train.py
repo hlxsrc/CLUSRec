ddd44b7aeb271b1a5d5355a528b9075b99aaa6b0
Hugo Rodriguez <hlxsrc@tutanota.com>
2021-10-05 21:22:57 -0500

    Use sparse categorical crossentropy
    Former-commit-id: b7fbcc8e6f8a9506bac96f60a460392c5879b5ee

--- singlelabel/train.py
+++ singlelabel/train.py
@@ -32,6 +32,8 @@ import argparse
 ap = argparse.ArgumentParser()
 ap.add_argument("-d", "--dataset", required=True, help="path to input dataset")
 ap.add_argument("-m", "--model", required=True, help="path to output model")
+ap.add_argument("-p", "--plot", type=str, default="plot.png",
+	help="path to output accuracy/loss plot")
 args = vars(ap.parse_args())
 
 # grab the list of images that we'll be describing
@@ -48,6 +50,8 @@ sdl = SimpleDatasetLoader(preprocessors=[sp, iap])
 (data, labels) = sdl.load(imagePaths, verbose=500)
 data = data.astype("float") / 255.0
 
+print("[INFO] labels... ", labels)
+
 # partition the data into training and testing splits using 75% of
 # the data for training and the remaining 25% for testing
 (trainX, testX, trainY, testY) = train_test_split(data, labels, test_size=0.25, random_state=42)
@@ -59,23 +63,27 @@ testY = LabelBinarizer().fit_transform(testY)
 # initialize the optimizer and model
 print("[INFO] compiling model...")
 opt = SGD(learning_rate=0.005)
-model = SmallerVGGNet.build(width=32, height=32, depth=3, classes=2, finalAct="sigmoid")
+model = SmallerVGGNet.build(width=32, height=32, depth=3, classes=2)
 
-model.compile(loss="binary_crossentropy", optimizer=opt, metrics=["accuracy"])
+model.compile(loss="sparse_categorical_crossentropy", optimizer=opt, metrics=["accuracy"])
+#model.compile(loss="categorical_crossentropy", optimizer=opt, metrics=["accuracy"])
+
+model.summary()
+
+# train the network
+print("[INFO] training network...")
+H = model.fit(trainX, trainY, validation_data=(testX, testY), batch_size=32, epochs=100, verbose=10)
+
+# Modify parameters accordingly
+BS = 32
+EPOCHS = 100
 
 # train the network
 #print("[INFO] training network...")
-#H = model.fit(trainX, trainY, validation_data=(testX, testY), batch_size=32, epochs=100, verbose=10)
-
-BS = 32
-EPOCHS = 100
-# train the network
-print("[INFO] training network...")
-H = model.fit(
-	trainX, trainY, batch_size=BS,
-	validation_data=(testX, testY),
-	steps_per_epoch=len(trainX) // BS,
-	epochs=EPOCHS, verbose=1)
+#H = model.fit(
+#	trainX, trainY, batch_size=BS,
+#	validation_data=(testX, testY),
+#	epochs=EPOCHS, verbose=10)
 
 # save the network to disk
 print("[INFO] serializing network...")
@@ -84,7 +92,7 @@ model.save(args["model"])
 # evaluate the network
 print("[INFO] evaluating network...")
 predictions = model.predict(testX, batch_size=32)
-print(classification_report(testY.argmax(axis=1), predictions.argmax(axis=1), target_names=["human", "other"]))
+print(classification_report(testY.argmax(axis=1), predictions.argmax(axis=1), target_names=["other", "person"]))
 
 # Starts cross validation
 print("\nCross Validation...")
@@ -107,9 +115,9 @@ for train, test in kfold.split(data, labels):
 
     # initialize the optimizer and model
     print("[INFO] compiling model...")
-    opt = SGD(lr=0.005)
+    opt = SGD(learning_rate=0.005)
     model = SmallerVGGNet.build(width=32, height=32, depth=3, classes=2, finalAct="sigmoid")
-    model.compile(loss="categorical_crossentropy", optimizer=opt, metrics=["accuracy"])
+    model.compile(loss="sparse_categorical_crossentropy", optimizer=opt, metrics=["accuracy"])
 
     # convert the labels from integers to vectors
     labels = LabelBinarizer().fit_transform(labels)
@@ -138,18 +146,19 @@ sns.heatmap(cm, annot=True, ax=ax, fmt='g', cmap='Greens')  # annot=True to anno
 ax.set_xlabel('Predicted labels')
 ax.set_ylabel('True labels')
 ax.set_title('Confusion Matrix')
-ax.xaxis.set_ticklabels(['human', 'other'])
-ax.yaxis.set_ticklabels(['human', 'other'])
+ax.xaxis.set_ticklabels(['other', 'person'])
+ax.yaxis.set_ticklabels(['other', 'person'])
 
 # plot the training loss and accuracy
 plt.style.use("ggplot")
 plt.figure()
 plt.plot(np.arange(0, 100), H.history["loss"], label="train_loss")
 plt.plot(np.arange(0, 100), H.history["val_loss"], label="val_loss")
-plt.plot(np.arange(0, 100), H.history["acc"], label="train_acc")
-plt.plot(np.arange(0, 100), H.history["val_acc"], label="val_acc")
+plt.plot(np.arange(0, 100), H.history["accuracy"], label="train_acc")
+plt.plot(np.arange(0, 100), H.history["val_accuracy"], label="val_acc")
 plt.title("Training Loss and Accuracy")
 plt.xlabel("Epoch #")
 plt.ylabel("Loss/Accuracy")
-plt.legend()
+plt.legend(loc="upper left")
+plt.savefig(args["plot"])
 plt.show()
