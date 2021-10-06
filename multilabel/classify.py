b01b0cb665d030e1f163e176f3e28aa56521b8d7
Hugo Rodriguez <hlxsrc@tutanota.com>
2021-10-05 21:20:56 -0500

    Save classified images

--- multilabel/classify.py
+++ multilabel/classify.py
@@ -1,10 +1,12 @@
 # USAGE
-# python classify.py --model clothes.model --labelbin mlb.pickle --image tests/test_01.jpg
+# python classify.py --image tests/test_01.jpg
 
 # import the necessary packages
+from configuration import config
 from tensorflow.keras.preprocessing.image import img_to_array
 from tensorflow.keras.models import load_model
 import numpy as np
+import mimetypes
 import argparse
 import imutils  
 import pickle
@@ -13,50 +15,79 @@ import os
 
 # construct the argument parse and parse the arguments
 ap = argparse.ArgumentParser()
-ap.add_argument("-m", "--model", required=True,
-	help="path to trained model model")
-ap.add_argument("-l", "--labelbin", required=True,
-	help="path to label binarizer")
-ap.add_argument("-i", "--image", required=True,
-	help="path to input image")
+#ap.add_argument("-m", "--model", required=True,
+#	help="path to trained model model")
+#ap.add_argument("-l", "--labelbin", required=True,
+#	help="path to label binarizer")
+ap.add_argument("-i", "--input", required=True,
+	help="path to input image/text file of image paths")
 args = vars(ap.parse_args())
 
-# load the image
-image = cv2.imread(args["image"])
-output = imutils.resize(image, width=400)
- 
-# pre-process the image for classification
-image = cv2.resize(image, (96, 96))
-image = image.astype("float") / 255.0
-image = img_to_array(image)
-image = np.expand_dims(image, axis=0)
+# determine the input file type, but assume that we're working with
+# single input image
+filetype = mimetypes.guess_type(args["input"])[0]
+imagePaths = [args["input"]]
+# if the file type is a text file, then we need to process *multiple*
+# images
+if "text/plain" == filetype:
+    # load the image paths in our testing file
+    imagePaths = open(args["input"]).read().strip().split("\n")
 
-# load the trained convolutional neural network and the multi-label
-# binarizer
+# load our object detector and label binarizer from disk
 print("[INFO] loading network...")
-model = load_model(args["model"])
-mlb = pickle.loads(open(args["labelbin"], "rb").read())
+model = load_model(config.MODEL_PATH)
+lb = pickle.loads(open(config.LB_PATH, "rb").read())
 
-# classify the input image then find the indexes of the two class
-# labels with the *largest* probability
-print("[INFO] classifying image...")
-proba = model.predict(image)[0]
-idxs = np.argsort(proba)[::-1][:2]
+total = 0
 
-print("Probability... ",proba)
-print("idxs: ", idxs)
+# loop over the images that we'll be testing using our bounding box
+# regression model
+for imagePath in imagePaths:
 
-# loop over the indexes of the high confidence class labels
-for (i, j) in enumerate(idxs):
-    # build the label and draw the label on the image
-    label = "{}: {:.2f}%".format(mlb.classes_[j], proba[j] * 100)
-    cv2.putText(output, label, (10, (i * 30) + 25), 
-    	cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 255, 0), 2)
+    # load the image
+    image = cv2.imread(imagePath)
+    output = imutils.resize(image, width=400)
+ 
+    # pre-process the image for classification
+    image = cv2.resize(image, (96, 96))
+    image = image.astype("float") / 255.0
+    image = img_to_array(image)
+    image = np.expand_dims(image, axis=0)
 
-# show the probabilities for each of the individual labels
-for (label, p) in zip(mlb.classes_, proba):
-    print("{}: {:.2f}%".format(label, p * 100))
+    # classify the input image then find the indexes of the two class
+    # labels with the *largest* probability
+    print("[INFO] classifying image...")
+    proba = model.predict(image)[0]
+    idxs = np.argsort(proba)[::-1][:2]
 
-# show the output image
-cv2.imshow("Output", output)
-cv2.waitKey(0)
+    # loop over the indexes of the high confidence class labels
+    for (i, j) in enumerate(idxs):
+        # build the label and draw the label on the image
+        label = "{}: {:.2f}%".format(lb.classes_[j], proba[j] * 100)
+        cv2.putText(output, label, (10, (i * 30) + 25), 
+    	    cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 255, 0), 2)
+
+    # show the probabilities for each of the individual labels
+    for (label, p) in zip(lb.classes_, proba):
+        print("{}: {:.2f}%".format(label, p * 100))
+
+    # show the output image
+    cv2.imshow("Output", output)
+    key = cv2.waitKey(0)
+
+    # if the `k` key was pressed, write the *original* frame to disk
+    # so we can later process it and use it for face recognition
+    if key == ord("k"):
+        p = "output/" + os.path.basename(imagePath)
+        print(p)
+        cv2.imwrite(p, output)
+        total += 1
+
+    # if the `q` key was pressed, break from the loop
+    elif key == ord("q"):
+        break
+
+# print the total faces saved and do a bit of cleanup
+print("[INFO] {} face images stored".format(total))
+print("[INFO] cleaning up...")
+cv2.destroyAllWindows()
