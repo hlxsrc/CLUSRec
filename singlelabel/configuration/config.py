4c0ace0bee5ca91bad8904ca630ccc5be124833a
Hugo Rodriguez <hlxsrc@tutanota.com>
2021-10-24 12:14:19 -0500

    Add config file for single-label training

--- singlelabel/configuration/config.py
+++ singlelabel/configuration/config.py
@@ -1,30 +1,48 @@
-# import the necessary packages
+# Import the necessary packages
 import os
 
-# define the base path to the input dataset and then use it to derive
-# the path to the images directory
-BASE_PATH = "../../datasets/bb"
-IMAGES_PATH = os.path.sep.join([BASE_PATH, "human"])
-ANNOTS_PATH = os.path.sep.join([BASE_PATH, "human_single.csv"])
-
-# define the path to the base output directory
-BASE_OUTPUT = "output"
-
-# define the path to the output serialized model, model training plot,
-# and testing image filenames
-MODEL_PATH = os.path.sep.join([BASE_OUTPUT, "detector.h5"])
-LB_PATH = os.path.sep.join([BASE_OUTPUT, "detector.pickle"])
-PLOT_PATH = os.path.sep.join([BASE_OUTPUT, "plot.png"])
-TEST_PATHS = os.path.sep.join([BASE_OUTPUT, "test_paths.txt"])
-TEST_FILENAMES = os.path.sep.join([BASE_OUTPUT, "test_paths.txt"])
-
-# Initialize the number of epochs to train for, initial learning rate,
-# batch size, and image dimensions
+# Initialize variables
+# TEST counter
+TEST = 1
+# DATASET name
+DATASET = "bb"
+# IMAGE_DIMS -> image dimesions (pre-processing)
+IMAGE_DIMS = (96, 96, 3)
 # EPOCHS -> in order to learn patterns using backpropagation (100 or 125)
 EPOCHS = 100
-# INIT_LR -> initial learning rate
-INIT_LR = 1e-4
+# LR -> initial learning rate
+LR = 1e-4
 # BS -> batch size
 BS = 32
-# IMAGE_DIMS -> image dimesions
-IMAGE_DIMS = (96, 96, 3)
+# Size of test in train_test_split
+TEST_SPLIT = 0.2
+
+# Define the base path to the input dataset
+BASE_PATH = "../../../datasets/" + DATASET
+
+# Add dir to complete base path
+IMAGES_PATH = os.path.sep.join([BASE_PATH, "images"])
+ANNOTS_PATH = os.path.sep.join([BASE_PATH, "annotations"])
+
+# Define the path to the base output directory
+BASE_OUTPUT = "output/" + DATASET
+
+# Create string to store name with the next notation
+# NOTATION: TEST # + DESCRIPTION + DATASET + IMAGE_DIMENSIONS + EPOCHS +
+#           LEARNING RATE + BATCH SIZE + TEST SPLIT
+model_path = str(TEST) + "_model_" + DATASET + "_" + str(IMAGE_DIMS[0]) + "_" + str(EPOCHS) + "_" + str(LR) + "_" + str(BS) + "_" + str(int(TEST_SPLIT*10)) + ".h5"
+lbin_path = str(TEST) + "_lbin_" + DATASET + "_" + str(IMAGE_DIMS[0]) + "_" + str(EPOCHS) + "_" + str(LR) + "_" + str(BS) + "_" + str(int(TEST_SPLIT*10)) + ".pickle"
+plot_path = str(TEST) + "_plot_" + DATASET + "_" + str(IMAGE_DIMS[0]) + "_" + str(EPOCHS) + "_" + str(LR) + "_" + str(BS) + "_" + str(int(TEST_SPLIT*10)) + ".png"
+accs_path = str(TEST) + "_accs_" + DATASET + "_" + str(IMAGE_DIMS[0]) + "_" + str(EPOCHS) + "_" + str(LR) + "_" + str(BS) + "_" + str(int(TEST_SPLIT*10)) + ".png"
+loss_path = str(TEST) + "_loss_" + DATASET + "_" + str(IMAGE_DIMS[0]) + "_" + str(EPOCHS) + "_" + str(LR) + "_" + str(BS) + "_" + str(int(TEST_SPLIT*10)) + ".png"
+test_path = str(TEST) + "_test_" + DATASET + "_" + str(IMAGE_DIMS[0]) + "_" + str(EPOCHS) + "_" + str(LR) + "_" + str(BS) + "_" + str(int(TEST_SPLIT*10)) + ".txt"
+
+# Define the path to the:
+#     output serialized model, labe binarizer, model training plots,
+#     and testing image filenames
+MODEL_PATH = os.path.sep.join([BASE_OUTPUT, model_path])
+LBIN_PATH = os.path.sep.join([BASE_OUTPUT, lbin_path])
+PLOT_PATH = os.path.sep.join([BASE_OUTPUT, plot_path])
+ACCS_PATH = os.path.sep.join([BASE_OUTPUT, accs_path])
+LOSS_PATH = os.path.sep.join([BASE_OUTPUT, loss_path])
+TEST_PATH = os.path.sep.join([BASE_OUTPUT, test_path])
--- singlelabel/train.py
+++ singlelabel/train.py
@@ -1,13 +1,12 @@
+# This scripts creates a CNN model using VGGNet to predict 
+# the class of images using bouding box regression
 # USAGE
-# python train.py
+# python train.py --config config
 
 # Enable/disable debugging logs (0,1,2,3)
 # 0 -> all, 3 -> none
 import os
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
-
-# Import confiuration file
-from configuration import config
 
 # Import the necessary packages
 from tensorflow.keras.preprocessing.image import ImageDataGenerator
@@ -31,18 +30,30 @@ import random
 import pickle
 import cv2
 
-# Construct the argument parse
-#ap = argparse.ArgumentParser()
-#ap.add_argument("-d", "--dataset", required=True,
-#   help="path to input dataset (directory of images)")
-#args = vars(ap.parse_args())
+# Construct the argument parse and parse the arguments
+ap = argparse.ArgumentParser()
+ap.add_argument("-c", "--config", required=True,
+   help="Path to configuration file")
+args = vars(ap.parse_args())
+
+# Handle configuration file (simple way)
+if not args["config"]:
+    config_file = "config"
+else:
+    config_file = args["config"]
+
+# Import configuration file
+cf = getattr(__import__("configuration", fromlist=[config_file]), config_file)
+
+# Configure output dir
+Path(cf.BASE_OUTPUT).mkdir(parents=True, exist_ok=True)
 
 # Disable eager execution
 tf.compat.v1.disable_eager_execution()
 
 # Load the contents of the CSV annotations file
 print("[INFO] loading dataset...")
-rows = open(config.ANNOTS_PATH).read().strip().split("\n")
+rows = open(cf.ANNOTS_PATH).read().strip().split("\n")
 
 # Initialize data (images), targets (bounding boxes) and filenames (images)
 data = []
@@ -57,7 +68,7 @@ for row in rows:
     (imageName, startX, startY, endX, endY) = row
 
     # Derive the path to the input image
-    imagePath = os.path.sep.join([config.IMAGES_PATH, imageName])
+    imagePath = os.path.sep.join([cf.IMAGES_PATH, imageName])
     # Load the image (in OpenCV format)
     image = cv2.imread(imagePath)
     # Grab its dimensions
@@ -71,7 +82,7 @@ for row in rows:
     endY = float(endY) / h
 
     # Load the image and preprocess it
-    image = load_img(imagePath, target_size=(config.IMAGE_DIMS[1], config.IMAGE_DIMS[0]))
+    image = load_img(imagePath, target_size=(cf.IMAGE_DIMS[1], cf.IMAGE_DIMS[0]))
     image = img_to_array(image)
 
     # Update our list of data, targets, and filenames
@@ -83,10 +94,9 @@ for row in rows:
 data = np.array(data, dtype="float32") / 255.0
 targets = np.array(targets, dtype="float32")
 
-# Partition the data into training and testing splits using 80% of
-# the data for training and the remaining 20% for testing
+# Partition the data into training and testing splits 
 split = train_test_split(data, targets,
-    imageNames, test_size=0.2, random_state=42)
+    imageNames, test_size=cf.TEST_SPLIT, random_state=42)
 
 # Unpack the data split
 (trainImages, testImages) = split[:2]
@@ -95,7 +105,7 @@ split = train_test_split(data, targets,
 
 # Write testing image paths to disk
 print("[INFO] saving testing image paths...")
-f = open(config.TEST_PATHS, "w")
+f = open(cf.TEST_PATH, "w")
 f.write("\n".join(testPaths))
 f.close()
 
@@ -104,21 +114,18 @@ f.close()
 #    height_shift_range=0.1, shear_range=0.2, zoom_range=0.2,
 #    horizontal_flip=True, fill_mode="nearest")
 
-# Initialize the model using a sigmoid activation as the final layer
-# in the network so we can perform multi-label classification
-# since it is single label we don't need number of classes
-# neither the finalAct
+# Initialize the model
 print("[INFO] compiling model...")
 model = SmallerVGGNet.build(
-    width=config.IMAGE_DIMS[1], height=config.IMAGE_DIMS[0],
-    depth=config.IMAGE_DIMS[2], classes=1,
+    width=cf.IMAGE_DIMS[1], height=cf.IMAGE_DIMS[0],
+    depth=cf.IMAGE_DIMS[2], classes=1,
     finalAct="sigmoid")
 
 # Initialize the optimizer (SGD)
-opt = Adam(lr=config.INIT_LR, decay=config.INIT_LR / config.EPOCHS)
+opt = Adam(lr=cf.LR, decay=cf.LR / cf.EPOCHS)
 
 # Compile the model using categorical cross-entropy
-#model.compile(loss="categorical_crossentropy", optimizer=opt,
+# model.compile(loss="categorical_crossentropy", optimizer=opt,
 #    metrics=["accuracy"])
 model.compile(loss="mse", optimizer=opt,
     metrics=["accuracy"])
@@ -131,16 +138,16 @@ print("[INFO] training bounding box regressor...")
 H = model.fit(
     trainImages, trainTargets,
     validation_data=(testImages, testTargets),
-    batch_size=config.BS,
-    epochs=config.EPOCHS, 
+    batch_size=cf.BS,
+    epochs=cf.EPOCHS, 
     verbose=1)
 
 # Save the model to disk
 print("[INFO] serializing network...")
-model.save(config.MODEL_PATH, save_format="h5")
+model.save(cf.MODEL_PATH, save_format="h5")
 
 # Plot the model training history
-N = config.EPOCHS
+N = cf.EPOCHS
 plt.style.use("ggplot")
 plt.figure()
 plt.plot(np.arange(0, N), H.history["loss"], label="train_loss")
@@ -149,4 +156,4 @@ plt.title("Bounding Box Regression Loss on Training Set")
 plt.xlabel("Epoch #")
 plt.ylabel("Loss")
 plt.legend(loc="lower left")
-plt.savefig(config.PLOT_PATH)
+plt.savefig(cf.PLOT_PATH)
