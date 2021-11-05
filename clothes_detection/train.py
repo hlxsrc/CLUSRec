a45cf37da5568c4dd55de9c98b298d76329aae91
Hugo Rodriguez <hlxsrc@tutanota.com>
2021-11-04 23:19:56 -0600

    Update training to use yaml configuration file

--- clothes_detection/train.py
+++ clothes_detection/train.py
@@ -1,6 +1,6 @@
 # This scripts creates a CNN model using VGGNet to predict classes of images.
 # USAGE
-# python train.py --config <configuration_file>
+# python train.py --configname <configuration>
 
 # Enable/disable debugging logs (0,1,2,3)
 # 0 -> all, 3 -> none
@@ -30,31 +30,33 @@ import pickle
 import cv2
 import os
 
+# Import configuration file
+from configuration import config
+
 # Construct the argument parse and parse the arguments
 ap = argparse.ArgumentParser()
-ap.add_argument("-c", "--config", required=True,
-   help="Path to configuration file")
+ap.add_argument("-cn", "--configname", required=True,
+   help="Name of desired configuration")
+ap.add_argument("-cf", "--configfile", required=False,
+   help="Name of configuration file")
 args = vars(ap.parse_args())
 
-# Handle configuration file (simple way)
-if not args["config"]:
-    config_file = "config"
-else:
-    config_file = args["config"]
+# Get selected configuration as dictionary
+config_dict = config.read_configuration(config=args["configname"])
 
-# Import configuration file
-cf = getattr(__import__("configuration", fromlist=[config_file]), config_file)
+# Create output paths
+paths_dict = config.create_paths(args["configname"], config_dict)
 
 # Configure output dir
-Path(cf.BASE_OUTPUT).mkdir(parents=True, exist_ok=True)
+Path(paths_dict["output"]).mkdir(parents=True, exist_ok=True)
 
 # Disable eager execution
 tf.compat.v1.disable_eager_execution()
 
 # Grab the image paths and randomly shuffle them
-print("[INFO] path: ", cf.IMAGES_PATH)
+print("[INFO] path: ", paths_dict["images"])
 print("[INFO] loading images...")
-imagePaths = sorted(list(paths.list_images(cf.IMAGES_PATH)))
+imagePaths = sorted(list(paths.list_images(paths_dict["images"])))
 random.seed(42)
 random.shuffle(imagePaths)
 
@@ -63,13 +65,16 @@ data = []
 imageNames = []
 labels = []
 
+# Get image dimensions
+IMAGE_DIMS = config_dict["imageDimensions"]
+
 # Loop over the input images
 for imagePath in imagePaths:
 	
     # Load the image, pre-process it, and store it in the data list
     image = cv2.imread(imagePath)
     # Pre-process the image
-    image = cv2.resize(image, (cf.IMAGE_DIMS[1], cf.IMAGE_DIMS[0]))
+    image = cv2.resize(image, (IMAGE_DIMS[1], IMAGE_DIMS[0]))
     image = img_to_array(image)
     # Update data list
     data.append(image)
@@ -100,7 +105,7 @@ for (i, label) in enumerate(mlb.classes_):
 
 # Partition the data into training and testing splits
 split = train_test_split(data, labels,
-    imageNames, test_size=cf.TEST_SPLIT, random_state=42)
+    imageNames, test_size=config_dict["testSplit"], random_state=42)
 
 # Unpack the data split
 (trainImages, testImages) = split[:2]
@@ -109,7 +114,7 @@ split = train_test_split(data, labels,
 
 # Write the testing image paths to disk
 print("[INFO] saving testing image paths...")
-f = open(cf.TEST_PATH, "w")
+f = open(paths_dict["test"], "w")
 f.write("\n".join(testPaths))
 f.close()
 
@@ -121,40 +126,42 @@ aug = ImageDataGenerator(rotation_range=25, width_shift_range=0.1,
 # Initialize the model 
 print("[INFO] compiling model...")
 model = SmallerVGGNet.build(
-	width=cf.IMAGE_DIMS[1], height=cf.IMAGE_DIMS[0],
-	depth=cf.IMAGE_DIMS[2], classes=len(mlb.classes_),
+	width=IMAGE_DIMS[1], height=IMAGE_DIMS[0],
+	depth=IMAGE_DIMS[2], classes=len(mlb.classes_),
 	finalAct="sigmoid")
 
 # Initialize the optimizer (SGD is sufficient)
-opt = Adam(learning_rate=cf.LR, decay=cf.LR / cf.EPOCHS)
+opt = Adam(learning_rate=config_dict["learningRate"], 
+        decay=config_dict["learningRate"] / config_dict["epochs"])
 
-# Compile the model using binary cross-entropy
+# Compile the model
 model.compile(loss="binary_crossentropy", optimizer=opt,
 	metrics=["accuracy"])
 
 # Train the network
 print("[INFO] training network...")
 H = model.fit(
-	x=aug.flow(trainImages, trainLabels, batch_size=cf.BS),
+	x=aug.flow(trainImages, trainLabels, 
+        batch_size=config_dict["batchSize"]),
 	validation_data=(testImages, testLabels),
-	steps_per_epoch=len(trainImages) // cf.BS,
-	epochs=cf.EPOCHS, verbose=10)
+	steps_per_epoch=len(trainImages) // config_dict["batchSize"],
+	epochs=config_dict["epochs"], verbose=10)
 
 # Save the model to disk
 print("[INFO] serializing network...")
-model.save(cf.MODEL_PATH, save_format="h5")
+model.save(paths_dict["model"], save_format="h5")
 
 # Save the multi-label binarizer to disk
 print("[INFO] serializing label binarizer...")
-f = open(cf.LBIN_PATH, "wb")
+f = open(paths_dict["binarizer"], "wb")
 f.write(pickle.dumps(mlb))
 f.close()
 
 # Evaluate the network
 print("\n")
 print("[INFO] Evaluating network...")
-predictions = model.predict(testImages, batch_size=32)
-print(classification_report(testLabels.argmax(axis=1), predictions.argmax(axis=1), target_names=["human", "other"]))
+predictions = model.predict(testImages, batch_size=config_dict["batchSize"])
+print(classification_report(testLabels.argmax(axis=1), predictions.argmax(axis=1), target_names=["black", "blue", "gray", "jeans", "pants", "shirt", "sweater", "white"]))
 
 # Create Confusion Matrix
 print("\n")
@@ -169,14 +176,14 @@ sns.heatmap(cm, annot=True, ax=ax, fmt='g', cmap='Greens')  # annot=True to anno
 ax.set_xlabel('Predicted labels')
 ax.set_ylabel('True labels')
 ax.set_title('Confusion Matrix')
-ax.xaxis.set_ticklabels(['human', 'other'])
-ax.yaxis.set_ticklabels(['human', 'other'])
-plt.savefig(cf.CMAT_PATH)
+ax.xaxis.set_ticklabels(["black", "blue", "gray", "jeans", "pants", "shirt", "sweater", "white"])
+ax.yaxis.set_ticklabels(["black", "blue", "gray", "jeans", "pants", "shirt", "sweater", "white"])
+plt.savefig(paths_dict["matrix"])
 
 # Plot the training loss and accuracy
 plt.style.use("ggplot")
 plt.figure()
-N = cf.EPOCHS
+N = config_dict["epochs"]
 plt.plot(np.arange(0, N), H.history["loss"], label="train_loss")
 plt.plot(np.arange(0, N), H.history["val_loss"], label="val_loss")
 plt.plot(np.arange(0, N), H.history["accuracy"], label="train_acc")
@@ -185,5 +192,5 @@ plt.title("Training Loss and Accuracy")
 plt.xlabel("Epoch #")
 plt.ylabel("Loss/Accuracy")
 plt.legend(loc="upper left")
-plt.savefig(cf.PLOT_PATH)
+plt.savefig(paths_dict["plot"])
 plt.show()
